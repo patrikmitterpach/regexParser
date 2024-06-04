4ef78790f06e808f2a1357449c2572a98fad0476
Patrik Mitterpach <patrik@mitterpach.com>
2024-06-04 21:22:12 +0200

    Update main for a runnable program

--- source/regexParser/main.py
+++ source/regexParser/main.py
@@ -1,5 +1,8 @@
 import regexParser.parsing as parsing
 import regexParser.matching as matching
+import regexParser.text_match as text
+
+import sys
 
 
 def parse(string: str):
@@ -9,3 +12,15 @@ def parse(string: str):
 def match(string: str, regex: str):
     parsed_regex = parsing.parse(regex)
     return matching.match(parsed_regex, string)
+
+
+def matchText(path: str, pattern: str):
+    return text.matchText(path, pattern)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        matchText("onShortnessOfLife.txt", sys.argv[1])
+    elif len(sys.argv) == 3:
+        matchText(sys.argv[2], sys.argv[1])
+    else:
+        matchText("onShortnessOfLife.txt", ".* ")
